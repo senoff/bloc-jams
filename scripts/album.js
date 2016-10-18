37a9cc30ab2f9c956c14ced60c0e79c532bd522d
Bob Senoff <bobsenoff@gmail.com>
2016-10-17 20:33:33 -0700

    Assignment 11 Completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,4 +1,5 @@
 // Example Album
+
 var albumPicasso = {
     title: 'The Colors',
     artist: 'Pablo Picasso',
@@ -22,6 +23,22 @@ var albumMarconi = {
     year: '1909',
     albumArtUrl: 'assets/images/album_covers/20.png',
     songs: [
+        {title: 'Born to Run', duration: '1:01' },
+        {title: 'Thunder Road', duration: '5:01' },
+        {title: 'A third song', duration: '3:21' },
+        {title: 'A fourth song', duration: '3:14' },
+        {title: 'A fifth song', duration: '2:15' }
+    ]
+};
+
+//Assignment - Third Album
+var albumBruce = {
+    title: 'Born to Run',
+    artist: 'Bruce Springsteen',
+    label: 'CBS Records International',
+    year: '1975',
+    albumArtUrl: 'assets/images/album_covers/Born_to_Run.png',
+    songs: [
         {title: 'Hello Operator?', duration: '1:01' },
         {title: 'Ring, ring, ring', duration: '5:01' },
         {title: 'First in your pocket', duration: '3:21' },
@@ -29,6 +46,7 @@ var albumMarconi = {
         {title: 'Wrong phone number', duration: '2:15' }
     ]
 };
+
 
 var createSongRow = function (songNumber, songName, songLength) {
     var template =
@@ -41,31 +59,39 @@ var createSongRow = function (songNumber, songName, songLength) {
     return template;
 };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function (album) {
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    console.log("albumTitle " + albumTitle);
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    // #2
+
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
     albumImage.setAttribute('src', album.albumArtUrl);
-
-    // #3
     albumSongList.innerHTML = '';
 
     // #4
     for (var i = 0; i < album.songs.length; i++) {
-        console.log("ASL " + album.songs.length);
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
 
 window.onload = function () {
-    console.log("Picasso onload" + albumPicasso);
     setCurrentAlbum(albumPicasso);
+
+    var albums = [albumPicasso, albumMarconi, albumBruce];
+
+    var index = 1;
+
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++
+        console.log("index" + index + "index.length" + index.length);
+        if (index == albums.length){
+            index = 0;
+        }
+        });
 };
