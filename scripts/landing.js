6f2df139d13bc382f274faa751dd9d40a7e3ceb4
Bob Senoff <bobsenoff@gmail.com>
2016-11-27 12:29:39 -0800

    Checkpoint 16

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,14 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function (points) {
-    var revealPoint = function (index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+    var revealPoint = function () {
+        // #7
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
     };
 
-    for ( var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+    // #6
+    $.each($('.point'), revealPoint);
 };
 
 $(window).load(function () {
